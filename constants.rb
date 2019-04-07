38caf3cd5a1a15adec64f3259b3e8b0c89c121fc
William Daly <william.daly@bbc.co.uk>
2019-04-07 11:39:12 +0100

    testing gui

---
+++ constants.rb
@@ -0,0 +1,10 @@
+puts "#################### Constants ####################"  
+
+William = 24
+puts "#{William}"
+
+# Note that Ruby will throw a warning since you are editing a constant
+# Constants start with a CAPITAL!
+
+William = 25
+puts "#{William}"
