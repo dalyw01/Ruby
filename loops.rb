df72d14ae76a48a3302bd1a48d85ddc765006bce
William Daly <william.daly@bbc.co.uk>
2020-02-20 14:45:49 +0000

    Update loops.rb

--- loops.rb
+++ loops.rb
@@ -1,5 +1,21 @@
 puts "#################### Loops ####################"  
 
+x = 0
+while x <= 100
+
+	if x % 5 == 0 and x % 3 == 0
+		puts "#{x} is a fizzbuzz"
+	elsif x % 5 == 0
+		puts "#{x} is a buzz"
+	elsif x % 3 == 0
+		puts "#{x} is a fizz"
+	else
+		puts "#{x} is NONE"
+	end
+	# puts x
+	x = x+1
+end
+
 # http://www.tutorialspoint.com/ruby/ruby_loops.htm
 puts "x.times all needs to be on the one line"
 
